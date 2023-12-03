d25df8a6870ad09b70297bce28e1e1a4c8b73d7b
rafa <rafa@Ubuntu.myguest.virtualbox.org>
2023-12-03 12:30:25 -0300

    criacao-rotas

--- loja/serializer.py
+++ loja/serializer.py
@@ -22,3 +22,22 @@ class ItemVendaSerializer(serializers.ModelSerializer):
     class Meta :
         model = ItemVenda
         fields = '__all__'
+
+class ListaItemVendaSerializer(serializers.ModelSerializer):
+    """Listar todos os produtos da venda 1 Vendas/1/Produtos"""
+    produto = serializers.ReadOnlyField(source='produto.nome')
+    vendedor = serializers.ReadOnlyField(source='vendedor.nome')
+    class Meta :
+        model = ItemVenda
+        fields = '__all__'
+    def get_ListaItemVendas(self, obj):
+        return obj.get_ListaItemVendas_display()
+    
+class ListarVendasVendedorSerializer(serializers.ModelSerializer):
+    vendedor_nome = serializers.ReadOnlyField(source='vendedor.nome')
+    cliente_nome = serializers.ReadOnlyField(source='cliente.nome')
+    class Meta :
+        model = Venda
+        fields = '__all__'
+        def get_VendasVendedor(self,obj):
+            return obj.get_vendasVendedor_display()
--- loja/views.py
+++ loja/views.py
@@ -1,6 +1,6 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, generics
 from loja.models import Cliente,GrupoProduto,Produto,Vendedor,Venda,ItemVenda
-from loja.serializer import VendaSerializer,ClienteSerializer,ProdutoSerializer,ItemVendaSerializer
+from loja.serializer import VendaSerializer,ClienteSerializer,ProdutoSerializer,ItemVendaSerializer, ListaItemVendaSerializer,ListarVendasVendedorSerializer
 
 class ClientesViewSet(viewsets.ModelViewSet):
     "exibir todos clientes"
@@ -21,3 +21,16 @@ class ItemVendaViewSet(viewsets.ModelViewSet):
     "exibir todos itens em uma venda"
     queryset = ItemVenda.objects.all()
     serializer_class = ItemVendaSerializer
+
+class ListaItemVendas(generics.ListAPIView):
+    def get_queryset(self):
+      queryset = ItemVenda.objects.filter(venda_id=self.kwargs['pk'])
+      return queryset
+    serializer_class = ListaItemVendaSerializer
+
+class ListaVendasVendedorView(generics.ListAPIView):
+    def get_queryset(self):
+        queryset = Venda.objects.filter(vendedor_id=self.kwargs['pk'])
+        return queryset
+    serializer_class = ListarVendasVendedorSerializer
+
